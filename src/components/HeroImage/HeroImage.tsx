5e1ed49ee9b042b327661c1ea93d9a5731bcbe90
David Stinnett <davidstinnett@icloud.com>
2023-05-15 13:48:27 -0400

    Update HeroImage.tsx

--- src/components/HeroImage/HeroImage.tsx
+++ src/components/HeroImage/HeroImage.tsx
@@ -63,17 +63,19 @@ const HeroImage: React.FC = () => {
   };
 
   const options: YouTubeProps["opts"] = {
-    height: "100%",
-    width: "100%",
-    playerVars: {
-      controls: 0,
-      loop: 1,
-      modestbranding: 1,
-      playsinline: 1,
-      mute: 1,
-      playlist: "FT3ODSg1GFE",
-    },
-  };
+  height: "100%",
+  width: "100%",
+  playerVars: {
+    autoplay: 1,
+    controls: 0,
+    loop: 1,
+    modestbranding: 1,
+    playsinline: 1,
+    mute: 1,
+    playlist: "FT3ODSg1GFE",
+  },
+};
+
 
   useEffect(() => {
     document.addEventListener("visibilitychange", handleVisibilityChange);
